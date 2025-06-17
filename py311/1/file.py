8e81c6861f9d7a1e1d3add43eb3295e3802ae2fa
bughug <59384277+bughug@users.noreply.github.com>
2025-06-17 11:34:21 +0800

    PEP 654

---
+++ py311/1/file.py
@@ -0,0 +1,70 @@
+from exception import (ExceptionGroup, OSError, SystemError, RecursionError, ZeroDivisionError, TypeError)
+
+def f():
+    raise ExceptionGroup(
+        "group1",
+        [
+            OSError(1),
+            SystemError(2),
+            ExceptionGroup(
+                "group2",
+                [
+                    OSError(3),
+                    RecursionError(4)
+                ]
+            )
+        ]
+    )
+
+try:
+    f()
+except* OSError as e:
+    print("There were OSErrors")
+except* SystemError as e:
+    print("There were SystemErrors")
+
+excs = []
+for test in tests:
+    try:
+        test.run()
+    except Exception as e:
+        excs.append(e)
+
+if excs:
+   raise ExceptionGroup("Test Failures", excs)
+
+
+def divide(x, y):
+    try:
+        result = x / y
+    except ZeroDivisionError:
+        print("division by zero!")
+    else:
+        print("result is", result)
+    finally:
+        print("executing finally clause")
+
+divide(2, 1)
+
+divide(2, 0)
+
+divide("2", "1")
+try:
+    raise TypeError('bad type')
+except Exception as e:
+    e.add_note('Add some information')
+    e.add_note('Add some more information')
+    raise
+
+def f():
+    raise OSError('operation failed')
+
+excs = []
+for i in range(3):
+    try:
+        f()
+    except Exception as e:
+        e.add_note(f'Happened in Iteration {i+1}')
+        excs.append(e)
+
+raise ExceptionGroup('We have some problems', excs)
